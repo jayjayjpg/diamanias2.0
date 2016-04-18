cf8b32d5a916dc383485067c28280fb9a984e69c
jessica-jordan <jessica.jordan.fe.dev@gmail.com>
2016-04-18 21:27:57 +0100

    add an interesting blur effect to index bg image on scroll

--- app/index/route.js
+++ app/index/route.js
@@ -4,6 +4,17 @@ export default Ember.Route.reopen({
   activate: function() {
     this._super();
     Ember.$('.body-canvas').addClass('index');
+    window.onscroll = function(e){
+      console.log(Ember.$('.index-panorama.blurred').length);
+      if (Ember.$('index-panorama.blurred').length === 0){
+        Ember.$('.index-panorama').addClass('blurred');
+        Ember.run.later(function(){
+          Ember.$('.index-panorama').removeClass('blurred');
+          console.log("remove");
+        }, 800);
+      }
+      return;
+    }
   },
   /* model(){
     return [
