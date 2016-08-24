17a8a5113423d75515a776824f64838bf0870dcc
jessica-jordan <jessica.jordan.fe.dev@gmail.com>
2016-08-24 21:22:46 +0100

    remove function statement from for loop and making JSHint more happy

--- app/components/moving-lines/component.js
+++ app/components/moving-lines/component.js
@@ -6,33 +6,35 @@ export default Ember.Component.extend({
   appendingLetter: "T",
   currentSeq: "",
   seqLen: 80,
+  count: 1,
+  init(){
+    this._super(...arguments);
+    this.writeBases();
+  },
   text: Ember.computed('appendingLetter', function(){
     let prolongedSeq = this.get('currentSeq') + this.get('appendingLetter');
     this.set('currentSeq', prolongedSeq);
-    console.log("change text: " + prolongedSeq);
+    this.set('count', this.get('count') + 1);
     return prolongedSeq;
   }),
   getBases: function(){
     const bases = ["T","G","A","C"];
     return bases[Math.floor(Math.random() * 4)];
   },
-  writeBases: function(){
-    if (!(this.get('isDestroyed') || this.get('isDestroying'))){
+  writeBases: Ember.observer('count', function(){
+   if (!(this.get('isDestroyed') || this.get('isDestroying'))){
       let self = this;
       let newLetter;
-      newLetter = self.getBases();
-      self.set('appendingLetter', newLetter);
-      console.log("appending letter " +  self.get("appendingLetter"));
-    }
-  },
-  didInsertElement(){
-      let self = this;
-      for (var i = 0; i < this.get('seqLen'); i += 1){
-      Ember.run.later(function(){
-          self.writeBases();
-        }, 50 * i);
+      if (this.get('count') < this.get('seqLen')){
+        Ember.run.later(function(){
+          self.set('appendingLetter', 'N'); // reset the appendingLetter prop to a random value so the newLetter value will definitely be different from the former randomly selected one. Hack to make this.set() execute
+          newLetter = self.getBases();
+          self.set('appendingLetter', newLetter);
+          console.log(self.get('count') + ": appending letter " +  self.get("appendingLetter"));
+        }, 50);
       }
-  },
+   }
+  }),
   willDestroyElement(){
     console.log("going into destruction mode....");
   }
